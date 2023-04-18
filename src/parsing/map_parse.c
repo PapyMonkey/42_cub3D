de659c814ea74dd7e0c9e936c4217d6845a700b0
Benjamin Gales <bjmgales@gmail.com>
2023-04-18 17:55:18 +0200

    build: first commit

---
+++ src/parsing/map_parse.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parse.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 16:32:00 by bgales            #+#    #+#             */
+/*   Updated: 2023/04/17 17:05:56 by bgales           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+void map_parse(char **map)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (map[++i])
+	{
+		j += itter_whitespace(&map[i][j]);
+		if (map[i][j] != '1')
+		{
+			printf("%c\n", map[i][j]);
+			print_and_exit("Error\nWrong map format.\n");
+		}
+		while (map[i][j])
+		{
+			if (in_set("10NEWS", map[i][j]))
+				j++;
+			else if (!map[i][j] || map[i][j] == '\n')
+			{
+				if (map[i][j - 1] != '1')
+					print_and_exit("Error\nWrong map format.\n");
+				break ;
+			}
+			else
+				print_and_exit("Error\nWrong map format.\n");
+		}
+		j = 0;
+	}
+}
---
+++ src/utils/parse_utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/13 16:30:27 by bgales            #+#    #+#             */
+/*   Updated: 2023/04/17 14:07:52 by bgales           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	NULL_all(t_game **game)
+{
+	(*game)->north = NULL;
+	(*game)->south = NULL;
+	(*game)->east = NULL;
+	(*game)->west = NULL;
+	(*game)->ceiling = NULL;
+	(*game)->floor = NULL;
+}
+int	in_set(char *set, char c)
+{
+	int	i;
+
+	i = -1;
+	while (set[++i])
+	{
+		if (set[i] == c)
+			return (1);
+	}
+	return (0);
+}
+void	is_cub(char *map_path)
+{
+	if (map_path[ft_strlen(map_path) - 4] != '.' || map_path[ft_strlen(map_path) - 3] != 'c' ||
+		map_path[ft_strlen(map_path) - 2] != 'u' || map_path[ft_strlen(map_path) - 1] !='b')
+		print_and_exit("Error\nMap must be to \".cub\" format\n");
+
+}
+
+int	itter_whitespace(char *str)
+{
+	int	i;
+
+	i = -1;
+	if (!str)
+		return (0);
+	while(str[++i] && (str[i] == ' ' || str[i] == '	' || str[i] == '\v'))
+	;
+	return (i);
+}
+
+int	itter_digit(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] >= '0' && str[i]<= '9')
+		i++;
+	return (i);
+}
