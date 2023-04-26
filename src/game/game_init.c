bff424d6e4bf095e0a5b8d41528b357b73b6c87a
Benjamin Gales <bjmgales@gmail.com>
2023-04-26 16:15:57 +0200

    feat: display map and player in 2d - player rotation + forward and backward movements.

--- src/game/game_init.c
+++ src/game/game_init.c
@@ -6,56 +6,65 @@
 /*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 12:10:15 by bgales            #+#    #+#             */
-/*   Updated: 2023/04/23 14:34:59 by bgales           ###   ########.fr       */
+/*   Updated: 2023/04/26 15:15:15 by bgales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	draw_map(t_game *game, int y, int x)
+void	draw_map(t_game *game, double y, double x)
 {
 	double	x_draw;
 	double	y_draw;
 
 	x_draw = x;
 	y_draw = y;
-	while (y_draw < y + 1)
+	while (y_draw < y + 0.9)
 	{
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x * 32, y_draw * 32, 16711680);
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, (x + 1) * 32, y_draw * 32, 16711680);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			x * 32, y_draw * 32, 16711680);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			(x + 0.9) * 32, y_draw * 32, 16711680);
 		y_draw += 0.01;
 	}
-	while (x_draw < x + 1)
+	while (x_draw < x + 0.9)
 	{
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x_draw * 32, y * 32, 16711680);
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x_draw * 32, (y + 1) * 32, 16711680);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			x_draw * 32, y * 32, 16711680);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			x_draw * 32, (y + 0.9) * 32, 16711680);
 		x_draw += 0.01;
 	}
 }
 
-void	draw_player(t_game *game, int i, int j)
+void	draw_player(t_game *game, double i, double j)
 {
 	double	x;
 	double	y;
 
 	x = j - 0.1;
 	y = i - 0.1;
-	while ((x += 0.01) < j)
+	while (x < j)
 	{
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x * 32, i * 32, 16777011);
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x * 32, y * 32, 16777011);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			(x + 0.5) * 32, (i + 0.5) * 32, 16777011);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			(x + 0.5) * 32, (y + 0.5) * 32, 16777011);
+		x += 0.01;
 	}
 	x = j - 0.1;
 	y = i - 0.1;
-	while ((y += 0.01) < i)
+	while (y < i)
 	{
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, j * 32, y * 32, 16777011);
-		mlx_pixel_put(game->mlx_ptr, game->win_ptr, x * 32, y * 32, 16777011);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			(j + 0.5) * 32, (y + 0.5) * 32, 16777011);
+		mlx_pixel_put(game->mlx_ptr, game->win_ptr,
+			(x + 0.5) * 32, (y + 0.5) * 32, 16777011);
+		y += 0.01;
 	}
-
 }
 
-void	mini_map(t_game *game)
+void	display(t_game *game)
 {
 	int	i;
 	int	j;
@@ -69,17 +78,31 @@ void	mini_map(t_game *game)
 			if (game->map[i][j] == '1')
 				draw_map(game, i, j);
 			if (in_set("NEWS", game->map[i][j]))
+			{
+				game->player->x = j;
+				game->player->y = i;
 				draw_player(game, i, j);
+			}
 		}
 		j = -1;
 	}
 }
+
+void	init_angles(t_game *game)
+{
+	game->player->pa = 2;
+	game->player->pdx = cos(game->player->pa) * 0.1;
+	game->player->pdy = sin(game->player->pa) * 0.1;
+}
+
 void	game_init(t_game **game)
 {
-
 	(*game)->mlx_ptr = mlx_init();
 	(*game)->win_ptr = mlx_new_window((*game)->mlx_ptr,
-		(((*game)->width - 1)) * 32, (((*game)->height) * 32), "cub3D");
-	mini_map(*game);
+			(((*game)->width - 1)) * 32, (((*game)->height) * 32), "cub3D");
+	(*game)->player = malloc(sizeof(t_player));
+	init_angles(*game);
+	display(*game);
+	mlx_hook((*game)->win_ptr, 02, 0L, key_events, *game);
 	mlx_loop((*game)->mlx_ptr);
 }
